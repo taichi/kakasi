c368b5e3a5bd0c9e2136d40e308bd6d4762e26eb
taichi <ryushi@gmail.com>
2018-01-04 11:38:46 +0900

    add echo.test

---
+++ test/command/echo.test.ts
@@ -0,0 +1,16 @@
+// tslint:disable-next-line:import-name
+import test, { TestContext } from 'ava';
+
+import { Echo } from '../../src/command/echo';
+import { Context } from '../../src/context';
+import { dummy } from '../../src/user';
+
+test((t: TestContext) => {
+    const cmd = ['aaa', 'bbb', 'ccc'];
+    const echo = new Echo(cmd);
+
+    return echo.execute(new Context(dummy()))
+        .then((s: string) => {
+            t.is(s, 'aaa bbb ccc');
+        });
+});
