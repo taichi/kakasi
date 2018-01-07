f9467f2b34d5e215d657e4ce1c36cebf8a023c1d
taichi <ryushi@gmail.com>
2018-01-08 03:50:46 +0900

    refactor

--- src/command/dict.ts
+++ src/command/dict.ts
@@ -21,7 +21,33 @@ export function editor(config: Config, cmd: string[]): Promise<ICommand> {
     }
 }
 
-export const KEY = 'command/InMemoryDict';
+export const KEY = 'command/dict/memory';
+
+// tslint:disable-next-line:no-multiline-string
+export const HELP = `
+    dict [list|add|delete|alias] ...
+      [list|ls] 辞書名
+        指定した辞書に登録されている語を列挙します。
+      [add|put|push] 辞書名 語
+        指定した辞書に語を登録します。
+      [delete|remove|del|rm] 辞書名 語
+        指定した辞書から語を削除します。
+      [alias|ln] 辞書名 辞書名
+        最初に指定した辞書名を、後に指定した辞書としても利用できるようにします。
+        最初に指定した辞書に語が登録されていない場合のみ使えます。
+      [help|?]
+        このヘルプを表示します。
+
+    コマンド例:
+      dict list 駅
+        駅という辞書に登録されている語を列挙します。
+      dict add 駅 新宿
+        駅という辞書に新宿という語を登録します。
+      dict rm 駅 新宿
+        駅という辞書から新宿という語を削除します。
+      dict ln 駅名 駅
+        駅という辞書を駅名という名前でも利用できるようにします。
+    `;
 
 export class InMemoryDict implements ICommand {
     private args: string[];
@@ -84,117 +110,90 @@ export class InMemoryDictEditor implements ICommand {
             case 'ln':
                 return this.alias(context);
             default:
-                return Promise.resolve(`dict コマンドには ${subcmd} というサブコマンドはありません。`);
+                return this.help(context);
         }
     }
 
     public help(context: Context): Promise<string> {
-        // tslint:disable-next-line:no-multiline-string
-        return Promise.resolve(`
-dict [list|add|delete|alias] ...
-  [list|ls] 辞書名
-    指定した辞書に登録されている語を列挙します。
-  [add|put|push] 辞書名 語
-    指定した辞書に語を登録します。
-  [delete|remove|del|rm] 辞書名 語
-    指定した辞書から語を削除します。
-  [alias|ln] 辞書名 辞書名
-    最初に指定した辞書名を、後に指定した辞書としても利用できるようにします。
-    最初に指定した辞書に語が登録されていない場合のみ使えます。
-  [help|?]
-    このヘルプを表示します。
-
-コマンド例:
-  dict list 駅
-    駅という辞書に登録されている語を列挙します。
-  dict add 駅 新宿
-    駅という辞書に新宿という語を登録します。
-  dict rm 駅 新宿
-    駅という辞書から新宿という語を削除します。
-  dict ln 駅名 駅
-    駅という辞書を駅名という名前でも利用できるようにします。
-`);
+        return Promise.resolve(HELP);
     }
 
     public list(context: Context): Promise<string> {
         const subargs = this.args.slice(1);
-        if (0 < subargs.length) {
-            const dict = context.get(KEY);
-            const key = subargs[0];
-            const words = dict.get(key);
-            if (words && 0 < words.length) {
-                const ls = words.join('\n');
+        if (subargs.length < 1) {
+            return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 列挙対象を指定するための引数が 1 つ必要です。`);
+        }
+        const dict = context.get(KEY);
+        const key = subargs[0];
+        const words = dict.get(key);
+        if (words && 0 < words.length) {
+            const ls = words.join('\n');
 
-                return Promise.resolve(ls);
-            }
-
-            return Promise.resolve(`${key} にはまだ何も登録されていません。`);
+            return Promise.resolve(ls);
         }
 
-        return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 列挙対象を指定するための引数が 1 つ必要です。`);
+        return Promise.resolve(`${key} にはまだ何も登録されていません。`);
     }
 
     public add(context: Context): Promise<string> {
         const subargs = this.args.slice(1);
-        if (1 < subargs.length) {
-            const dict = context.get(KEY);
-            const [key, newone] = subargs;
-            let words = dict.get(key);
-            if (!words) {
-                words = [];
-                dict.set(key, words);
-            }
-            const found = words.find((v: string) => v === newone);
-            if (found) {
-                return Promise.reject(`${key} に ${newone} は登録済みです。`);
-            }
-            words.push(newone);
-
-            return Promise.resolve(`${key} に ${newone} を登録しました。`);
+        if (subargs.length < 2) {
+            return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 登録対象の辞書及び登録する語の 2 つの引数が必要です。`);
         }
+        const dict = context.get(KEY);
+        const [key, newone] = subargs;
+        let words = dict.get(key);
+        if (!words) {
+            words = [];
+            dict.set(key, words);
+        }
+        const found = words.find((v: string) => v === newone);
+        if (found) {
+            return Promise.reject(`${key} に ${newone} は登録済みです。`);
+        }
+        words.push(newone);
 
-        return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 登録対象の辞書及び登録する語の 2 つの引数が必要です。`);
+        return Promise.resolve(`${key} に ${newone} を登録しました。`);
+
     }
 
     public remove(context: Context): Promise<string> {
         const subargs = this.args.slice(1);
-        if (1 < subargs.length) {
-            const dict = context.get(KEY);
-            const [key, exists] = subargs;
-            const words = dict.get(key);
-            if (!words || words.length < 1) {
-                return Promise.reject(`${key} という辞書が無いか、それに登録された語がありません。`);
-            }
-
-            const newone = words.filter((s: string) => s !== exists);
-            dict.set(key, newone);
-
-            return Promise.resolve(`${key} から ${exists} を削除しました。`);
+        if (subargs.length < 2) {
+            return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 削除対象の辞書及び削除する語の 2 つの引数が必要です。`);
+        }
+        const dict = context.get(KEY);
+        const [key, exists] = subargs;
+        const words = dict.get(key);
+        if (!words || words.length < 1) {
+            return Promise.reject(`${key} という辞書が無いか、それに登録された語がありません。`);
         }
 
-        return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 削除対象の辞書及び削除する語の 2 つの引数が必要です。`);
+        const newone = words.filter((s: string) => s !== exists);
+        dict.set(key, newone);
+
+        return Promise.resolve(`${key} から ${exists} を削除しました。`);
     }
 
     public alias(context: Context): Promise<string> {
         const subargs = this.args.slice(1);
-        if (1 < subargs.length) {
-            const dict = context.get(KEY);
-            const [newone, exists] = subargs;
-            const words = dict.get(newone);
-            if (words && 0 < words.length) {
-                return Promise.reject(`${newone} には語が登録済みです。語が登録されていない辞書のみを指定できます。`);
-            }
-
-            const existsWords = dict.get(exists);
-            if (!existsWords || existsWords.length < 1) {
-                return Promise.reject(`${exists} には語が登録されていません。語が登録されている辞書のみを指定できます。`);
-            }
-
-            dict.set(newone, existsWords);
-
-            return Promise.resolve(`${newone} を ${exists} としても使えるようになりました。`);
+        if (subargs.length < 2) {
+            return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 新しい辞書名と既存の辞書名の 2 つの引数が必要です。`);
+        }
+        const dict = context.get(KEY);
+        const [newone, exists] = subargs;
+        const words = dict.get(newone);
+        if (words && 0 < words.length) {
+            return Promise.reject(`${newone} には語が登録済みです。語が登録されていない辞書のみを指定できます。`);
         }
 
-        return Promise.reject(`dictコマンドの ${this.args[0]} サブコマンドには 新しい辞書名と既存の辞書名の 2 つの引数が必要です。`);
+        const existsWords = dict.get(exists);
+        if (!existsWords || existsWords.length < 1) {
+            return Promise.reject(`${exists} には語が登録されていません。語が登録されている辞書のみを指定できます。`);
+        }
+
+        dict.set(newone, existsWords);
+
+        return Promise.resolve(`${newone} を ${exists} としても使えるようになりました。`);
     }
 }
