0b070bfb875bbc8a0ab464d7439830a2d948be1b
taichi <ryushi@gmail.com>
2018-01-17 21:39:28 +0900

    remove unused code

--- src/tx.sqlite.ts
+++ src/tx.sqlite.ts
@@ -2,32 +2,6 @@ import { Context } from './context';
 
 import * as sqlite from 'sqlite';
 
-export function transaction(contextkey: string, index: number = 0) {
-    // tslint:disable-next-line:no-any
-    return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
-        const originalMethod = descriptor.value;
-
-        // tslint:disable-next-line:no-any
-        descriptor.value = async function (...args: any[]) {
-            const ctx: Context = args[index];
-            const db: sqlite.Database = ctx.get(contextkey);
-            await db.exec('begin transaction');
-            try {
-                // tslint:disable-next-line:no-invalid-this
-                const result = await originalMethod.apply(this, args);
-                await db.exec('commit');
-
-                return result;
-            } catch (e) {
-                await db.exec('rollback');
-                throw e;
-            }
-        };
-
-        return descriptor;
-    };
-}
-
 export async function doTransaction<T>(db: sqlite.Database, op: () => Promise<T>): Promise<T> {
     await db.exec('begin transaction');
     try {
